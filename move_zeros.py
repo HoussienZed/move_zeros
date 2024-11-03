257a7cf1adb69e4ef85bd1025c048a8768851d74
Houssien Zeineddine <houessienzd@hotmail.com>
2024-11-03 21:39:42 +0200

    setting the input code

--- move_zeros.py
+++ move_zeros.py
@@ -9,3 +9,15 @@ def move_zeros(numbers):
 
     moved_zeros_list = no_zeros + zeros
     return (print(f"The moved zero list is {moved_zeros_list}"))
+
+# Entering list of numbers
+
+
+numbers = int(input("Enter the length of numbers list: "))
+unarranged_numbers = []
+for i in range(numbers):
+    number = int(input(f"Enter number {i + 1}: "))
+    unarranged_numbers.append(number)
+
+print(f"The unarranged list of numbers is= {unarranged_numbers}")
+move_zeros(unarranged_numbers)
